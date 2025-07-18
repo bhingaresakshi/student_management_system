082ed8bd9d486bcaa225e2b6af5b85107c853486
Sakshi Bhingare <146076512+bhingaresakshi@users.noreply.github.com>
2025-07-19 00:20:59 +0530

    Add files via upload

---
+++ custom.py
@@ -0,0 +1,11 @@
+# --- Color Palette (harmonizing with deep sky blue) ---
+color_1 = "#00bfff"   
+color_2 = "#f5faff"   
+color_3 = "#222831"  
+color_4 = "#ffffff"   
+
+
+# --- Fonts ---
+font_1 = ("Times New Roman", 16, "bold")       # Titles
+font_2 = ("Helvetica", 13, "normal")           # Labels, input fields
+
